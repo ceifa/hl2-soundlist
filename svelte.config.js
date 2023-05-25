8865e729c9acd940a5e24bedbad9be2182e1fcf1
ceifa <gabrinelson27@gmail.com>
2023-05-25 13:43:18 -0300

    cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,9 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			edge: true
-		})
+		adapter: adapter()
 	}
 };
 
